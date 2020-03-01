78a36dad60144d6b4f587426e6d44a03194f234b
Ken Faulkner <ken.faulkner@gmail.com>
2020-03-01 13:11:44 -0700

    precious init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kpfaulkner/precious
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kpfaulkner/precious/models"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	eventPath := os.Getenv("GITHUB_EVENT_PATH")
+  fmt.Printf("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx\n")
+
+	dat, err := ioutil.ReadFile(eventPath)
+	if err != nil {
+    fmt.Printf("unable to read event")
+    return
+	}
+
+	// have the data, deserialise
+  var ev models.GollumEventModel
+	err = json.Unmarshal(dat, &ev)
+	if err != nil {
+		fmt.Printf("cannot unmarshal event data")
+		return
+	}
+
+  for _,page := range ev.Pages {
+  	fmt.Printf("page title: %s , pagename: %s, star: %d\n", page.Title, page.PageName, ev.Repository.StargazersCount)
+  }
+
+	//fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+}
